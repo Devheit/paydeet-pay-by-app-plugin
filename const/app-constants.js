5edd09414a473d5743288db5b5a893def1b94bb4
Bello Olamilekan <belloolamilekan661@gmail.com>
2024-11-10 17:30:10 +0100

    Added .js file extension to fix error on imports in dummy ecommerce site using type module import syntax

--- const/app-constants.js
+++ const/app-constants.js
@@ -1,14 +1,14 @@
-export const appUrl = 'http://localhost:5174/';
+export const appUrl = "http://localhost:5173/";
 
-export const appRootId = 'pay-by-app';
+export const appRootId = "pay-by-app";
 
-export const frameInClass = 'frame-in';
+export const frameInClass = "frame-in";
 
-export const frameOutClass = 'frame-out';
+export const frameOutClass = "frame-out";
 
 export const validCurrencyMap = {
-  USD: 'USD',
-  EUR: 'EUR',
-  GBP: 'GBP',
-  INR: 'INR',
+  USD: "USD",
+  EUR: "EUR",
+  GBP: "GBP",
+  INR: "INR",
 };

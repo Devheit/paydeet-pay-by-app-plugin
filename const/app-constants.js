ec478681b4756051b92f10058a7a0df7c18e5d80
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-04-04 13:52:06 +0100

    change appUrl endpoint

--- const/app-constants.js
+++ const/app-constants.js
@@ -1,7 +1,7 @@
 import currencies from "../data/currencies.js";
 
 export const appUrl =
-  "https://staging.d1hcmrwpxa0xw3.amplifyapp.com/" || "http://localhost:5173/";
+  "https://staging.d2baixouvqfg1m.amplifyapp.com/" || "http://localhost:5173/";
 
 export const appRootId = "pay-by-app";
 

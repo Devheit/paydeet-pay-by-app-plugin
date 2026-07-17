40deaccd6714cb9d8b14c5bcac3a5fb178fa3222
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2026-07-17 16:53:34 +0100

    fix: main app url

--- const/app-constants.js
+++ const/app-constants.js
@@ -1,6 +1,6 @@
 import currencies from "../data/currencies.js";
 export const appUrl =
-  "https://main.d1b5971hafz61h.amplifyapp.com/" || "http://localhost:5173/";
+  "https://rpzh0vb9c0.execute-api.eu-west-1.amazonaws.com/prod" || "http://localhost:5173/";
 
 export const appRootId = "pay-by-app";
 
